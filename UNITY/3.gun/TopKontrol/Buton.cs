02e879b9ae7c47782c95dc1ed6eea13ce00e6caa
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2023-07-05 00:13:30 +0300

    Add files via upload

---
+++ UNITY/3.gun/TopKontrol/Buton.cs
@@ -0,0 +1,11 @@
+using System. Collections;
+using System. Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buton : MonoBehaviour {
+
+public void YenidenBasla (){
+        SceneManager.LoadScene("oyun");
+    }
+}
