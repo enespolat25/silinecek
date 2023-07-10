936e89b7af5833d8203e2b40b8162dff0879c1d1
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2023-07-10 22:48:06 +0300

    Add files via upload

---
+++ UNITY/3.gun/CarController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+
+}
+
+public class CarController : MonoBehaviour
+{
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+    public float acceleration;
+
+    private void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical") * acceleration;
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach(AxleInfo axleInfo in axleInfos)
+        {
+            if(axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if(axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+        }
+    }
+
+
+
+
+}
